04958458d359f4860bbbec494f85bce7f4caec11
Iraponan Marinho <iraponan.aires@gmail.com>
2024-04-22 20:40:32 -0300

    Configurando e testando o projeto.

---
+++ lib/config/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+mixin ThemeProject {
+  static final theme = ThemeData(
+    primarySwatch: Colors.blue,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:xlo_mobx/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AppXLOMobx());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
