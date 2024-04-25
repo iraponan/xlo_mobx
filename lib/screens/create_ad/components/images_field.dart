a6f16fa470271c68df83d466993ac8223bb223db
Iraponan Marinho <iraponan.aires@gmail.com>
2024-04-24 23:28:18 -0300

    Começando o campo de imagens.

---
+++ lib/screens/create_ad/components/images_field.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ImagesField extends StatelessWidget {
+  const ImagesField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      height: 120,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+            child: GestureDetector(
+              onTap: () {
+                if (Platform.isIOS) {
+                  showCupertinoModalPopup(
+                      context: context, builder: (context) => Container());
+                } else {
+                  showModalBottomSheet(
+                      context: context, builder: (context) => Container());
+                }
+              },
+              child: CircleAvatar(
+                radius: 44,
+                backgroundColor: Colors.grey[300],
+                child: const Icon(
+                  Icons.add_a_photo,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/screens/create_ad/create_ad_screen.dart
+++ lib/screens/create_ad/create_ad_screen.dart
@@ -2,6 +2,7 @@ import 'package:brasil_fields/brasil_fields.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:xlo_mobx/components/drawer/custom_drawer.dart';
+import 'package:xlo_mobx/screens/create_ad/components/images_field.dart';
 
 class CreateAdScreen extends StatelessWidget {
   const CreateAdScreen({super.key});
@@ -24,10 +25,12 @@ class CreateAdScreen extends StatelessWidget {
       body: Center(
         child: SingleChildScrollView(
           child: Card(
+            clipBehavior: Clip.antiAlias,
             margin: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
+                const ImagesField(),
                 TextFormField(
                   decoration: const InputDecoration(
                     labelText: 'Título *',
